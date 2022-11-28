97338b472ec771aad97154e59398d40dd9b8208a
Benterf123 <faithbenter@gmail.com>
2022-11-28 20:27:09 +0000

    print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let myVariable1 = 'C is fun';
+let myVariable2 = 'Python is cool';
+let myVariable3 = 'JavaScript is amazing';
+console.log(myVariable1);
+console.log(myVariable2);
+console.log(myVariable3);
